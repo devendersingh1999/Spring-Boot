25881f8091c2a11719ae94281b90be67440b78a0
devendersingh1999 <devendersingh29121999@gmail.com>
2024-10-07 19:49:28 +0530

    Add files via upload
    Storing data in MySQL database

---
+++ StudentSqlDb/src/main/java/com/example/demo/StudentSqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSqlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSqlDbApplication.class, args);
+	}
+
+}
